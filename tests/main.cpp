3411585eceb936880cec2011ab5a811b45e46629
Mohammed Al-Mahdawi <m.ghaithalmahdawi@gmail.com>
2025-02-15 18:52:41 +0100

    Changed Test Function Name

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,6 @@
 #include "1.hpp"    // Tests yaml variables 
 
 int main(int argc, char** argv) {
-    TINY_YAML::tests::test_variables();
+    TINY_YAML::tests::test_1_variables();
     return 0;
 }
